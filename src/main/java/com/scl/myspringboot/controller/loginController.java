b84c1f7afd98dba343935010e5a6f1dabd34ef8f
58467 <584670891@qq.com>
2020-03-03 14:52:35 +0800

    Springboot初始环境搭建

---
+++ src/main/java/com/scl/myspringboot/controller/loginController.java
@@ -0,0 +1,16 @@
+package com.scl.myspringboot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author shangchunlong
+ * @create 2020-03-03 10:48
+ */
+@RestController
+public class loginController {
+    @RequestMapping(value = "/login")
+    public  String  login(){
+        return "index";
+    }
+}
